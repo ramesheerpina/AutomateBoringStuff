573114a02c23a5beab5bd0aafd0492b3d268bdb7
Ramesh <ramesh.eerpina@gmail.com>
2019-04-23 22:15:57 -0400

    Generating random numbers using randint function and math module

--- AutomateBoringStuff.py
+++ AutomateBoringStuff.py
@@ -1,19 +1,3 @@
-print("my name is")
-for i in range(5):
-    print("Jimmy five Times "+ str(i))
-
-total = 0
-for number in range(100):
-    total = total+number
-print(total)
-
-wtotal=0
-while wtotal < 100:
-    wtotal = wtotal+1
-    print(wtotal)
-
-total=0
-for i in range(0,100, 2):
-    print(i)
-    total=total+i
-print(total)
+import random
+for i in range(3):
+    print(random.randint(1,5))
