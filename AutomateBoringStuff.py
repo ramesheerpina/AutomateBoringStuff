5ec98fdecbb8e9478a786ac4b37ba3b39bca9390
reerpina <ramesh.eerpina@gmail.com>
2019-04-24 10:19:07 -0400

    functiond - using return statement

--- AutomateBoringStuff.py
+++ AutomateBoringStuff.py
@@ -1,6 +1,28 @@
-def hello(name):
-    print("hi "+name)
-    print("Howdy "+name)
-    print("Hello there "+ name)
+#priint statment prints the input given to it where as the return statment stores the value
+#in the function where can be used later. If a print statment is used in the function it
+#will print the value either the function is called or not where as the return will print
+#only if the function is called.
+import random
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'It is certain'
+    elif answerNumber == 2:
+        return'It is decidedly so'
+    elif answerNumber == 3:
+        return 'Yes'
+    elif answerNumber == 4:
+        return 'Reply hazy try again'
+    elif answerNumber == 5:
+        return 'Ask again later'
+    elif answerNumber == 6:
+        return 'Concentrate and ask again'
+    elif answerNumber == 7:
+        return 'My reply is no'
+    elif answerNumber == 8:
+        return 'Outlook not so good'
+    elif answerNumber == 9:
+        return 'Very doubtful'
 
-hello("John")
+r = random.randint(1,10)
+fortune = getAnswer(r)
+print(fortune)
