ba6a9744ec9800d468c2a73b9b42e543cbb7269b
reerpina <ramesh.eerpina@gmail.com>
2019-04-24 10:23:27 -0400

    functions - difference between print and return statements

--- AutomateBoringStuff.py
+++ AutomateBoringStuff.py
@@ -2,27 +2,13 @@
 #in the function where can be used later. If a print statment is used in the function it
 #will print the value either the function is called or not where as the return will print
 #only if the function is called.
-import random
-def getAnswer(answerNumber):
-    if answerNumber == 1:
-        return 'It is certain'
-    elif answerNumber == 2:
-        return'It is decidedly so'
-    elif answerNumber == 3:
-        return 'Yes'
-    elif answerNumber == 4:
-        return 'Reply hazy try again'
-    elif answerNumber == 5:
-        return 'Ask again later'
-    elif answerNumber == 6:
-        return 'Concentrate and ask again'
-    elif answerNumber == 7:
-        return 'My reply is no'
-    elif answerNumber == 8:
-        return 'Outlook not so good'
-    elif answerNumber == 9:
-        return 'Very doubtful'
+def number(num):
+    print(num)
+    print("last "+ str(num) + " came from print statement")
+    return(num)
 
-r = random.randint(1,10)
-fortune = getAnswer(r)
-print(fortune)
+number(9)
+
+print("lets see what is printed")
+
+print(number(10))
