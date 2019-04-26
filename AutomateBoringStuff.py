f4c4dfc078fdbf28613a48c06f6acc6f2d2d62ff
reerpina <ramesh.eerpina@gmail.com>
2019-04-26 11:56:26 -0400

    Function - Collatz program

--- AutomateBoringStuff.py
+++ AutomateBoringStuff.py
@@ -1,14 +1,7 @@
-#priint statment prints the input given to it where as the return statment stores the value
-#in the function where can be used later. If a print statment is used in the function it
-#will print the value either the function is called or not where as the return will print
-#only if the function is called.
-def number(num):
-    print(num)
-    print("last "+ str(num) + " came from print statement")
-    return(num)
-
-number(9)
-
-print("lets see what is printed")
-
-print(number(10))
+def collatz(number):
+    if number%2 == 0:
+        return(number//2)
+    else:
+        return(3*number+1)
+num = int(input("Enter a number: "))
+print(collatz(num))
