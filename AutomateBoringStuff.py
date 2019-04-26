a021761e6184b2f930fd8b586c6a84e130e00b46
reerpina <ramesh.eerpina@gmail.com>
2019-04-26 11:56:48 -0400

    Function - Collatz program with try and except

--- AutomateBoringStuff.py
+++ AutomateBoringStuff.py
@@ -3,5 +3,16 @@ def collatz(number):
         return(number//2)
     else:
         return(3*number+1)
-num = int(input("Enter a number: "))
-print(collatz(num))
+
+if __name__=="__main__":
+    print("enter number: ")
+    try:
+        number = int(input())
+        while True:
+            if number != 1:
+                number = collatz(number)
+                print(number)
+            else:
+                break
+    except:
+        print("Error: Invalid Value. You did ot enter an integer")
